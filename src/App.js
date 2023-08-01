ecedbb6419ed0c9f07c67cb647ed27d88e0fbe21
jorget0rr3s <torresor.jt@gmail.com>
2023-08-01 15:10:41 -0500

    changes styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Container from './Componentes/Container';
 function App() {
   return (
     <div className="App">
+      <h2 className='title'> What´s the plan for today? </h2>
       <Container />
     </div>
   );
